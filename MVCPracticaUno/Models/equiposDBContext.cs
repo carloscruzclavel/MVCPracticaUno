6f8c02f456a336c72a45aca2e176a17e563dbe57
2020CC605 <2020CC605@unicaeselsalvador0.onmicrosoft.com>
2023-03-25 14:53:17 -0600

    nueva actualizacion

--- MVCPracticaUno/Models/equiposDBContext.cs
+++ MVCPracticaUno/Models/equiposDBContext.cs
@@ -7,9 +7,10 @@ namespace MVCPracticaUno.Models
     public class equiposDBContext : DbContext
     {
         public equiposDBContext(DbContextOptions options) : base(options) {
-        
-            
-        
+      
         }
+
+        public DbSet<marcas> marcas { get; set; }
+
     }
 }
---
+++ MVCPracticaUno/Models/marcas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCPracticaUno.Models
+{
+    public class marcas
+    {
+        [Key]
+        public int id_marcas { get; set; }
+        [Display(Name = "Nombre de la marca")]
+        public string? nombre_marca { get; set; }
+        [Display(Name = "Estado de la Marca (A: Activo, I: Inactivo)")]
+        public string? estados { get; set; }
+    }
+}
